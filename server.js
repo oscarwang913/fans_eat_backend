7436cf9e34a0353848c92c578c7e000deea4ebd0
Oscar Wang <garnett284@gmail.com>
2021-04-13 09:50:41 +0800

    cors issue modify

--- server.js
+++ server.js
@@ -11,16 +11,7 @@ const routes = require("./routes/index.route");
 app.use(helmet());
 app.use(cors());
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
-  );
-  next();
-});
+app.options("*", cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(logger("dev"));
