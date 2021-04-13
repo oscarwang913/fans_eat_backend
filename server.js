e05684c382d0e6383c5faaa4873844adecff1020
Oscar Wang <garnett284@gmail.com>
2021-04-13 10:10:22 +0800

    cors

--- server.js
+++ server.js
@@ -9,13 +9,19 @@ const app = express();
 const server = http.createServer(app);
 const routes = require("./routes/index.route");
 app.use(helmet());
-app.use(cors());
+let allowCrossDomain = function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  next();
+};
 
+app.use(cors());
 app.options("*", cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(logger("dev"));
 // user `use` to ensure the get all http req
+app.use(allowCrossDomain);
 app.use("/api", routes);
 
 server.listen(PORT, () => {
